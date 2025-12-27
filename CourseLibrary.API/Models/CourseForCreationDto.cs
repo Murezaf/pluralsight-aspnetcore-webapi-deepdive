bbe29e8952e1997c87ff9d545dacd555546c8758
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2025-12-27 10:57:11 +0330

    Customize error messages

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -4,11 +4,11 @@ namespace CourseLibrary.API.Models;
 
 public class CourseForCreationDto
 {
-    [Required]
-    [MaxLength(100)]
+    [Required(ErrorMessage = "You should fillout the title.")]
+    [MaxLength(100, ErrorMessage = "Title can't have more than 100 characters.")]
     public string Title { get; set; } = string.Empty;
-    [MaxLength(1500)]
+    
+    [MaxLength(1500, ErrorMessage = "Description can't have more than 1500 characters.")]
     //public string Description { get; set; } = string.Empty;
     public string? Description { get; set; } = string.Empty; //allow Description to be null
 }
-
